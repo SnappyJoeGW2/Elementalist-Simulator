2a1ad73be5190863125d8a7f438889fce02db6da
SnappyJoe <krzysztofkapera95@gmail.com>
2026-03-29 11:56:33 +0200

    Version up

--- js/optimizer.js
+++ js/optimizer.js
@@ -9,7 +9,7 @@
 // Each non-gear combo (rune × relic × sigil pair × food × utility × infusions)
 // is sent to a Web Worker for parallel exhaustive evaluation.
 
-import { SimulationEngine } from './simulation.js?v=10';
+import { SimulationEngine } from './simulation.js?v=11';
 import { calcAttributes } from './calc-attributes.js';
 import { GEAR_SLOTS, GEAR_STATS, WEAPON_DATA, getActiveGearSlots } from './gear-data.js';
 
